d7ed2714a5b56359f9137c1d1b5b7f787c78ec8f
Ashraf59 <ashrafams59b@gmail.com>
2022-11-24 21:58:06 +0600

    Data created and get data from MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
 
 const app = express();
 
@@ -10,8 +13,27 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.USER_DB}:${process.env.USER_PASS}@cluster0.l1ydak8.mongodb.net/?retryWrites=true&w=majority`;
+// console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
 async function run (){
-    
+    try{
+        const productsCollection = client.db('mobileBazar').collection('products');
+
+        app.get('/products', async(req, res) =>{
+            const query = {}
+            const result = await productsCollection.find(query).toArray();
+            res.send(result)
+
+        })
+    }
+    finally{
+
+    }
 }
 run().catch(console.log);
 
