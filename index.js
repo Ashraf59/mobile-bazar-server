9e6312ed4cf699dd84035c5cf059260b5b5ff606
Ashraf59 <ashrafams59b@gmail.com>
2022-11-25 14:26:11 +0600

    Category data insert to MongoDB and fetch from MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors')
 const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -23,12 +23,21 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run (){
     try{
         const productsCollection = client.db('mobileBazar').collection('products');
+        const categoryCollection = client.db('mobileBazar').collection('category');
 
         app.get('/products', async(req, res) =>{
             const query = {}
             const result = await productsCollection.find(query).toArray();
             res.send(result)
 
+        });
+
+        app.get('/category/:name', async(req, res) => {
+            const name = req.params.name;
+            console.log(name);
+            const filter = {name}
+            const result = await categoryCollection.find(filter).toArray();
+            res.send(result)
         })
     }
     finally{
