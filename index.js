d3725bd996bb0ed693e7d5c0403ed2a365ddecb5
Ashraf59 <ashrafams59b@gmail.com>
2022-11-28 01:08:07 +0600

    Advertise data post to MongoDB

--- index.js
+++ index.js
@@ -117,6 +117,14 @@ async function run (){
             res.send(users);
         });
 
+        // User info from mongodb database
+        app.delete('/users/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) }
+            const result = await usersCollection.deleteOne(filter);
+            res.send(result);
+        });
+
         app.get('/users/admin/:email', async (req, res) => {
             const email = req.params.email;
             const query = { email }
